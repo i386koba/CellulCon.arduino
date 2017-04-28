e99cc403f20148fc781e20580cc9f12bc64f577a
i386koba <i386koba@gmail.com>
2017-04-28 11:46:58 +0900

    GPSモジュール http://akizukidenshi.com/catalog/g/gK-09991/ を追加

--- CellularRC.arduino.ino
+++ CellularRC.arduino.ino
@@ -22,6 +22,8 @@
   http://i.stack.imgur.com/oOtkU.png
   PWM　３，５，６，９，１０，１１Pin
 */
+#include <SoftwareSerial.h>
+SoftwareSerial gpsSerial(15, 16); // RX, TX
 
 #include <Servo.h>
 Servo sv2;
@@ -43,6 +45,8 @@ int lasthallVal = 0;     // 前回読み取った値を保持する変数
 int hallCount = 0; //ホール通過カウント
 unsigned long lastTime;
 boolean servoAttached = false;
+byte gpsArray[100];
+int gpsIndex = 0;
 
 void setup()
 {
@@ -54,10 +58,51 @@ void setup()
   //Arduino 新機能 pinMode関数の第2引数にINPUT_PULLUP http://mag.switch-science.com/2013/05/23/input_pullup/
   pinMode(inPin, INPUT_PULLUP);// ホール素子用に入力に設定
   lastTime = millis();
+  gpsSerial.begin(9600); // ソフトウェアシリアルの初期化
 }
 
 void loop()
 {
+  //GPS文字列比較
+  //ＧＰＳ受信機キット　１ＰＰＳ出力付き　「みちびき」対応
+  //http://akizukidenshi.com/catalog/g/gK-09991/
+  //GPSのNMEAフォーマット
+  //http://www.hiramine.com/physicalcomputing/general/gps_nmeaformat.html
+  //例
+  //$GPGGA,062950.000,3550.4017,N,13757.4716,E,2,11,0.81,652.2,M,37.7,M,0000,0000*6C
+  //$GPGLL,3550.4017,N,13757.4716,E,062950.000,A,D*56
+  //$GPGSA,A,3,23,09,08,27,31,21,26,11,07,193,16,,1.51,0.81,1.28*3E
+  //$GPGSV,4,1,13,27,77,327,26,16,60,047,19,08,49,253,44,42,47,167,30*74
+  //$GPGSV,4,2,13,26,41,084,17,21,25,049,42,23,20,247,20,07,20,318,36*73
+  //$GPGSV,4,3,13,09,18,282,36,11,11,211,22,31,11,149,29,193,07,175,35*4F
+  //$GPGSV,4,4,13,18,03,086,*4F
+  //$GPRMC,062950.000,A,3550.4017,N,13757.4716,E,0.02,67.20,270417,,,D*57
+  //$GPVTG,67.20,T,,M,0.02,N,0.03,K,D*0A
+  //$GPZDA,062950.000,27,04,2017,,*5B
+
+  if (gpsSerial.available()) {
+    int d = gpsSerial.read();
+    byte s;
+    if (d > 0) {
+      s = byte(d);
+    }
+    
+    if ( s == '$' ) {
+      gpsIndex = 0;
+    }
+    gpsArray[gpsIndex++] = s;
+    
+    if ( s == 10 && gpsIndex != 0 ) { //ニューライン(ASCIIコード10あるいは'\n') 
+      //"$GPGGA" 検証
+      if (gpsArray[3] == 'G' && gpsArray[4] == 'G' && gpsArray[5] == 'A' ) {
+       Serial.write(gpsArray, gpsIndex);
+       //Serial.flush(); 
+      }
+    }
+     
+  }
+
+
   //TODO attachInterrupt(interrupt, function, mode) http://www.musashinodenpa.com/arduino/ref/index.php?f=0&pos=3045
   //Arduino Hall Effect Sensor Project http://diyhacking.com/projects/ArduinoHallEffect.ino
   hallVal = digitalRead(inPin);  // 入力ピンを読む
@@ -122,10 +167,11 @@ void loop()
         Serial.println("RPS:" + String(hallCount, DEC));
         //"Servo: " + String(sv9.read(), DEC) + " : " + String(sv10.read(), DEC));
         hallCount = 0;
-      } else { //A3 Batt Volte 
+      } else { //A3 Batt Volte
         int batValue = analogRead(A3);
         Serial.println("BAT:" + String(batValue, DEC));
       }
+     
     }
     else if ( '0' <= d && d <= '9' ) {
       d -= '0';
